2d62fbe07ae56136b93844d82a158733363a595e
admin <admin@mail.ru>
2021-12-19 16:03:05 +0300

    v1

--- mail.tf
+++ mail.tf
@@ -8,10 +8,10 @@ terraform {
 }
 
 provider "yandex" {
-  token     = "<AQAAAABKlbEIAATuwWvtTyeyv0mOklLnlQVvaac>"
-  cloud_id  = "<b1gm7ni2v808puh04hm5>"
-  folder_id = "<b1gr2ctfcm32jom2pged>"
-  zone      = "<ru-central1-b>"
+  token     = "AQAAAABKlbEIAATuwWvtTyeyv0mOklLnlQVvaac"
+  cloud_id  = "b1gm7ni2v808puh04hm5"
+  folder_id = "b1gr2ctfcm32jom2pged"
+  zone      = "ru-central1-b"
 }
 
 resource "yandex_compute_instance" "build" {
