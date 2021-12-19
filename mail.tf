f75610a7975c31477f48fb81853f76f640513f92
admin <admin@mail.ru>
2021-12-19 16:10:22 +0300

    v1

--- mail.tf
+++ mail.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     yandex = {
       source  = "yandex-cloud/yandex"
-      version = "0.61.0"
+      version = "0.68.0"
     }
   }
 }
@@ -19,26 +19,4 @@ resource "yandex_compute_instance" "build" {
   platform_id = "standard-v1"
   zone = "ru-central1-b"
   }
-
-  resources {
-    cores  = 2
-    memory = 4
-  }
-  
-  boot_disk {
-    initialize_params {
-      image_id = "image_id"
-    }
-  }
-
-  network_interface {
-    subnet_id = "${yandex_vpc_subnet.foo.id}"
-  }
-
-resource "yandex_vpc_network" "foo" {}
-
-resource "yandex_vpc_subnet" "foo" {
-  zone       = "ru-central1-b"
-  network_id = "${yandex_vpc_network.foo.id}"
-}
 
