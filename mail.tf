6bbe95b2365f716de7fdc3daaefb95d39c13698a
User <ws@mail.ru>
2021-12-20 13:54:10 +0300

    v1

--- mail.tf
+++ mail.tf
@@ -34,7 +34,7 @@ resource "yandex_compute_instance" "bui" {
         nat       = true
   }
   metadata = {
-  ssh-keys = "ubuntu:${file("root/.ssh/ilya.pub")}"
+  ssh-keys = "ubuntu:${file("~/.ssh/ilya.pub")}"
   }
 }
 
